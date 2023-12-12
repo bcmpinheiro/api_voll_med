022f46f6e589f7b9f2091302d97ac2b9d02f6766
Barbara Pinheiro <bcmpinheiro@gmail.com>
2023-12-12 16:20:28 -0300

    primeiro commit

---
+++ src/main/java/med/voll/api/domain/endereco/DadosEndereco.java
@@ -0,0 +1,20 @@
+package med.voll.api.domain.endereco;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record DadosEndereco (
+
+
+    @NotBlank
+    String logradouro,
+    @NotBlank
+    String bairro,
+    @NotBlank
+    String cep,
+    @NotBlank
+    String cidade,
+    @NotBlank
+    String uf,
+    String complemento,
+    String numero){
+}
